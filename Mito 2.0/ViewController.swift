f4ef0ad211dddc2c103d84696f89f0ca39d06df7
neliSun <nelson.chow91.nc@gmail.com>
2017-03-20 17:21:46 -0700

    Initial Commit

---
+++ Mito 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mito 2.0
+//
+//  Created by Nelson Chow on 2017-03-20.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
